51663b73d5dcecab07aadfc3118140ed43ee9fc1
LiuJingXX <1165645506@qq.com>
2018-12-27 10:21:51 +0800

    Initial commit

---
+++ src/Ljj/AplusB.java
@@ -0,0 +1,14 @@
+package Ljj;
+
+/**
+ * @ClassName AplusB
+ * @Description TODO
+ * @Author LiuJing
+ * @Date 2018/12/27 10:02
+ * @Version 1.0
+ **/
+public class AplusB {
+    public static void main(String[] args) {
+        System.out.println(3 + 2);
+    }
+}
